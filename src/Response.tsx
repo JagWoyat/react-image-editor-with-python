4dd2d91e579d92a3561c98e8e958761ab991d06f
JagWoyat <jagwoyat@tutanota.com>
2023-07-08 15:08:20 +0200

    added navigation buttons

--- src/Response.tsx
+++ src/Response.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Response() {
   const [image, setImage] = useState<any>();
@@ -14,9 +15,23 @@ export default function Response() {
     });
   }, []);
 
+  const handleDownload = () => {
+    if (!params.filename) return;
+    const link = document.createElement("a");
+    link.href = "./../server/output/" + params.filename;
+    link.download = params.filename;
+    link.click();
+  };
+
   return (
-    <div id="response-page">
+    <div className="ResponseWrapper">
       <img src={image} alt="response image" />
+      <div className="ResponseDiv">
+        <Link to="/">
+          <button>Return</button>
+        </Link>
+        <button onClick={handleDownload}>Download</button>
+      </div>
     </div>
   );
 }
