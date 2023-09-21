d6e13736dc80e9bac14165084cd7ce49cd94a6d5
JagWoyat <jagwoyat@tutanota.com>
2023-09-21 19:02:23 +0200

    IMG-10 added customization to Icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,16 +1,5 @@
-import { Link } from "react-router-dom";
 import styles from "./Icon.module.css";
 
-export default function Icon({ icon, linkTo }: any) {
-  return (
-    <Link to={linkTo}>
-      <button
-        style={{
-          backgroundColor: "transparent",
-          backgroundImage: `url(${icon})`,
-        }}
-        className={styles.Button}
-      ></button>
-    </Link>
-  );
+export default function Icon({ IconStyles }: any) {
+  return <button style={IconStyles} className={styles.Button}></button>;
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,27 @@
 import Icon from "../Icon/Icon";
 import styles from "./Navbar.module.css";
 import icon from "../../img/image-circle-plus-svgrepo-com.svg";
+import { Link } from "react-router-dom";
+
+const IconStyles = {
+  marginTop: "1.5rem",
+  marginRight: "0.5rem",
+  width: "4rem",
+  height: "4rem",
+  backgroundColor: "transparent",
+  backgroundImage: `url(${icon})`,
+};
 
 const Navbar = ({ children }: any) => {
   return (
     <>
       <div className={styles.Navbar}>
-        <div className={styles.NameWrapper}>
-          <Icon icon={icon} linkTo="/" />
-          <h1>Image Editor</h1>
-        </div>
+        <Link to={"/"}>
+          <div className={styles.NameWrapper}>
+            <Icon IconStyles={IconStyles} />
+            <h1>Image Editor</h1>
+          </div>
+        </Link>
       </div>
 
       {children}
